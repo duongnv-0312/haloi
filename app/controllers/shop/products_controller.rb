94c59685e58f0c40a2952f1346e2fd3ea3e8a34a
oNgoVanDuong <ngo.van.duong@framgia.com>
2018-03-26 23:47:13 +0700

    post products with multiple pictures

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -10,12 +10,20 @@ class Shop::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
+    attach_pictures if @product.save
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :image)
+    params.require(:product).permit(:name, :price)
+  end
+
+  def attach_pictures
+    if params[:images]
+      params[:images].each do |image|
+        @product.pictures.create(image: image)
+      end
+    end
   end
 end
---
+++ app/models/picture.rb
@@ -0,0 +1,11 @@
+class Picture < ApplicationRecord
+
+  belongs_to :product
+
+  has_attached_file :image,
+    path: ":rails_root/public/images/:id/:filename",
+    url: "/images/:id/:filename",
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
+end
