5a1dcf01ce58b16ed2cb50db5f464e5123fd58b6
oNgoVanDuong <bapboy18@yahoo.com.vn>
2019-11-17 14:56:38 +0700

    User login via facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "products#index"
 
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   namespace :shop do
     resources :products
